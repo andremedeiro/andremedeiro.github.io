d744e1f01e7f129e0574a8f71e25d4c72f82c286
andremedeiro <32900190+andremedeiro@users.noreply.github.com>
2021-02-23 18:05:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var scroll = document.getElementById('scroll');
-let doc = document.documentElement;
+const doc = document.documentElement;
 
 window.addEventListener('scroll', function() {
 	var value = parseInt(100 * doc.scrollTop / (doc.scrollHeight - doc.clientHeight));
 	scroll.style.width = value+'%';
 });
 
+$('#lista-menu a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset
+	}, 600);
+});
+
+var chat = new ZenviaChat('8af8687f5426a6f23ae1ee6ba6019f8b')
+.embedded('buttonless')
+.height('500px')
+.build()
